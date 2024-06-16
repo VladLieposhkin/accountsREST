b971164b509805f783e2bb8b03dffb2f16462a6b
Vlad Lieposhkin <Vlad.Lieposhkin@gmail.com>
2024-06-16 17:59:25 +0100

    init commit

---
+++ src/main/java/vl/example/accountsrest/AccountsRestApplication.java
@@ -0,0 +1,13 @@
+package vl.example.accountsrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountsRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountsRestApplication.class, args);
+    }
+
+}
